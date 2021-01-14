bc82a2d5286e06e3480c6bd114f26ab4dfff418e
JingYu Ning <foldfelis@gmail.com>
2021-01-14 20:41:39 +0800

    trim code base

--- sqstate/main.py
+++ sqstate/main.py
@@ -1,55 +1,43 @@
 import os
-import sys
 
-import tensorflow as tf
 import numpy as np
 import scipy.io as sio
+import tensorflow as tf
 from tensorflow.keras import backend as bak
-from tensorflow.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.layers import Dropout, Input, Dense, Conv1D, AveragePooling1D, Flatten, BatchNormalization, add, \
     Activation
-from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.models import Model
 from tensorflow.keras.utils import get_custom_objects
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 
 
-def mock_process():
-    np.set_printoptions(threshold=10)
+def custom_activation(x):
+    return bak.relu(x) * 1
 
-    reduce_lr = ReduceLROnPlateau(
-        monitor='loss', factor=0.7, patience=5, mode='auto', min_delta=0.0000001, cooldown=10, min_lr=0.0000005, verbose=1
-    )
+
+def custom_loss(y_true, y_pred):
+    mse = bak.mean(bak.square(y_true - y_pred), axis=-1)
+    sum_constraint = 0.002 * bak.abs(1 * (bak.sum(bak.square(y_pred), axis=-1) - 1))
+    return mse + sum_constraint
+
+
+def get_model():
     opt = tf.keras.optimizers.Adam(learning_rate=0.0001, beta_1=0.7, beta_2=0.9)
-
-
-    def custom_activation(x):
-        return bak.relu(x) * 1
-
-
     get_custom_objects().update({'custom_activation': Activation(custom_activation)})
 
-
-    def custom_loss(y_true, y_pred):
-        # Normal MSE loss
-        mse = bak.mean(bak.square(y_true - y_pred), axis=-1)
-
-        sum_constraint = 0.002 * bak.abs(1 * (bak.sum(bak.square(y_pred), axis=-1) - 1))
-
-        return mse + sum_constraint
-
-
     homodyne_time_in = Input(shape=(4032, 1), name='homodyne_time')
-
     homodyne_time0 = Conv1D(128, 4, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time_in)
     homodyne_time0 = BatchNormalization()(homodyne_time0)
     homodyne_time0 = Activation(custom_activation, name='SpecialActivation1')(homodyne_time0)
-    homodyne_time1 = Conv1D(64, 1, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time1 = Conv1D(64, 1, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time0)
     homodyne_time1 = BatchNormalization()(homodyne_time1)
     homodyne_time1 = Activation(custom_activation, name='SpecialActivation2')(homodyne_time1)
-    homodyne_time1 = Conv1D(64, 4, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time1 = Conv1D(64, 4, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time1)
     homodyne_time1 = BatchNormalization()(homodyne_time1)
     homodyne_time1 = Activation(custom_activation, name='SpecialActivation3')(homodyne_time1)
@@ -62,11 +50,13 @@ def mock_process():
 
     homodyne_time2 = AveragePooling1D(pool_size=2)(homodyne_time2)
 
-    homodyne_time3 = Conv1D(64, 1, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time3 = Conv1D(64, 1, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time2)
     homodyne_time3 = BatchNormalization()(homodyne_time3)
     homodyne_time3 = Activation(custom_activation, name='SpecialActivation5')(homodyne_time3)
-    homodyne_time3 = Conv1D(64, 4, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time3 = Conv1D(64, 4, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time3)
     homodyne_time3 = BatchNormalization()(homodyne_time3)
     homodyne_time3 = Activation(custom_activation, name='SpecialActivation6')(homodyne_time3)
@@ -76,11 +66,13 @@ def mock_process():
     homodyne_time3 = BatchNormalization()(homodyne_time3)
     homodyne_time3 = Activation(custom_activation, name='SpecialActivation7')(homodyne_time3)
     homodyne_time4 = add([homodyne_time2, homodyne_time3])
-    homodyne_time5 = Conv1D(64, 1, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time5 = Conv1D(64, 1, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time4)
     homodyne_time5 = BatchNormalization()(homodyne_time5)
     homodyne_time5 = Activation(custom_activation, name='SpecialActivation8')(homodyne_time5)
-    homodyne_time5 = Conv1D(64, 4, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time5 = Conv1D(64, 4, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time5)
     homodyne_time5 = BatchNormalization()(homodyne_time5)
     homodyne_time5 = Activation(custom_activation, name='SpecialActivation9')(homodyne_time5)
@@ -93,11 +85,13 @@ def mock_process():
 
     homodyne_time6 = AveragePooling1D(pool_size=8)(homodyne_time6)
 
-    homodyne_time7 = Conv1D(64, 1, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time7 = Conv1D(64, 1, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time6)
     homodyne_time7 = BatchNormalization()(homodyne_time7)
     homodyne_time7 = Activation(custom_activation, name='SpecialActivation11')(homodyne_time7)
-    homodyne_time7 = Conv1D(64, 4, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time7 = Conv1D(64, 4, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time7)
     homodyne_time7 = BatchNormalization()(homodyne_time7)
     homodyne_time7 = Activation(custom_activation, name='SpecialActivation12')(homodyne_time7)
@@ -107,11 +101,13 @@ def mock_process():
     homodyne_time7 = BatchNormalization()(homodyne_time7)
     homodyne_time7 = Activation(custom_activation, name='SpecialActivation13')(homodyne_time7)
     homodyne_time8 = add([homodyne_time6, homodyne_time7])
-    homodyne_time9 = Conv1D(64, 1, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time9 = Conv1D(64, 1, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time8)
     homodyne_time9 = BatchNormalization()(homodyne_time9)
     homodyne_time9 = Activation(custom_activation, name='SpecialActivation14')(homodyne_time9)
-    homodyne_time9 = Conv1D(64, 4, strides=1, padding="same", kernel_initializer=tf.random_normal_initializer(stddev=0.001),
+    homodyne_time9 = Conv1D(64, 4, strides=1, padding="same",
+                            kernel_initializer=tf.random_normal_initializer(stddev=0.001),
                             bias_initializer='zeros')(homodyne_time9)
     homodyne_time9 = BatchNormalization()(homodyne_time9)
     homodyne_time9 = Activation(custom_activation, name='SpecialActivation15')(homodyne_time9)
@@ -214,17 +210,18 @@ def mock_process():
 
     model = Model([homodyne_time_in], [DTX_out])
 
-    print(model.summary())
-
     model.compile(
         optimizer=opt,
         loss=custom_loss,
         metrics=['accuracy', 'mae', 'mape', 'mse']
     )
-
     model.load_weights(f'{CURRENT_PATH}/my_model_weights_0107.h5')
 
-    # model = load_model(f'{CURRENT_PATH}/my_model_(1).h5')
+    return model
+
+
+def get_1d_density_matrix():
+    model = get_model()
 
     matfn7 = f'{CURRENT_PATH}/test_data_010701.mat'
     data7 = sio.loadmat(matfn7)
@@ -242,4 +239,4 @@ def mock_process():
     # sio.savemat('density_mtx_pred_i_0107.mat', mdict={'density_mtx_pred_i': density_mtx_pred_i})
     # sio.savemat('density_mtx_pred_r_0107.mat', mdict={'density_mtx_pred_r': density_mtx_pred_r})
 
-    return example_result_if.shape
+    return density_mtx_pred_r, density_mtx_pred_i
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,7 @@
-from sqstate.main import mock_process
+from sqstate.main import get_1d_density_matrix
 
 
-def test_mock_process():
-    assert mock_process() == (1, 630)
+def test_get_1d_density_matrix():
+    density_matrix_r, density_matrix_i = get_1d_density_matrix()
+    assert density_matrix_r.shape == (1, 630)
+    assert density_matrix_i.shape == (1, 630)
