b153bda23e84717fe86e62cf35336d312f380d1c
JingYu Ning <foldfelis@gmail.com>
2021-01-16 16:03:07 +0800

    fix reshape bug and add test case

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,6 @@
 from sqstate.preprocess import preprocess
 from sqstate.model import get_model
-from sqstate.postprocess import postprocess
+from sqstate.postprocess import PostProcessor
 from sqstate.utils import ArrayPrinter
 
 
@@ -8,13 +8,13 @@ def main():
     data = preprocess("test_data_010701.mat")
     model = get_model("my_model_weights_0107.h5")
     result = model.predict(data)
-    real, imag, dm = postprocess(result, 35)
 
-    print("Real part:")
-    ArrayPrinter(real, [7, 28, 10, 25]).print()
+    postprocessor = PostProcessor(result, 35)
+    postprocessor.run()
+    l_ch, dm = postprocessor.l_ch, postprocessor.density_matrix
 
-    print("Imag part:")
-    ArrayPrinter(imag, [7, 28, 10, 25]).print()
+    print("L:")
+    ArrayPrinter(l_ch, [5, 32, 10, 25]).print()
 
     print("Density Matrix:")
     ArrayPrinter(dm, [5, 32, 10, 25]).print()
