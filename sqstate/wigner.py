fa471e88b1cc3b2d6b5b9a4072eb0fd788a969e6
JingYu Ning <foldfelis@gmail.com>
2021-01-25 16:24:06 +0800

    remove recurrence implement

--- sqstate/wigner.py
+++ sqstate/wigner.py
@@ -1,14 +1,4 @@
 def laguerre(x, n, alpha):
-    # by recurrence
-    # if n == 0:
-    #     return 1
-    # elif n == 1:
-    #     return -x + alpha + 1
-    # elif n == 2:
-    #     return (x**2 / 2) - ((alpha + 2) * x) + ((alpha + 2) * (alpha + 1) / 2)
-    # else:
-    #     return ((2*n + 1 + alpha - x) * laguerre(x, n-1, alpha) - (n + alpha) * laguerre(x, n-1, alpha)) / (n + 1)
-
     # by Horner's method
     laguerre_l = 1
     b = 1
